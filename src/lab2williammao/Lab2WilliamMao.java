ff1999999e4325fa86ac429ff4b55e68505aade2
William Mao <weilong.mao@edu.sait.ca>
2022-09-15 13:54:51 -0600

    Update Added code for this project.

--- src/lab2williammao/Lab2WilliamMao.java
+++ src/lab2williammao/Lab2WilliamMao.java
@@ -15,7 +15,7 @@ public class Lab2WilliamMao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Derek!");
     }
     
 }
