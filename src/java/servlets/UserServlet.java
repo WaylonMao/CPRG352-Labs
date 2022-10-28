ba48841a99dac68078b6653ead421cdf157c87c8
William Mao <weilong.mao@edu.sait.ca>
2022-10-27 22:50:18 -0600

    All functions are implemented.

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -70,7 +70,29 @@ public class UserServlet extends HttpServlet {
                 String message = us.add(user);
                 req.setAttribute("message", message);
                 break;
-
+            }
+            case "delete": {
+                String email = req.getParameter("email");
+                // Just trying to avoid using JSTL :)
+                String message = "Are you sure about that? "
+                        + "<form method='post'><input type='hidden' name='action' value='remove' />"
+                        + "<input type='hidden' name='email' value='" + email + "' />"
+                        + "<input type='submit' value='Confirm' /></form><form method='post'>"
+                        + "<input type='hidden' name='action' value='cancel' />"
+                        + "<input type='submit' value='Cancel' /></form>";
+                req.setAttribute("message", message);
+                break;
+            }
+            case "remove": {
+                String email = req.getParameter("email");
+                UserService us = new UserService();
+                String message = us.delete(us.get(email));
+                req.setAttribute("message", message);
+                break;
+            }
+            default:{
+                req.setAttribute("message", "Please don't hack me -_-! Thank you!");
+                break;
             }
         }
         UserService us = new UserService();
